168db54691f1288651fb74668299419a6af85191
oyedeletemitope <meettopsy5fc@gmail.com>
2023-12-26 08:33:32 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./newStyle.css";
+const HomePage = () => {
+  const [coins, setCoins] = useState([]);
 
-function App() {
+  // Fetch coins data from API
+  const fetchCoins = () => {
+    fetch("https://api.coingecko.com/api/v3/search/trending")
+      .then((res) => res.json())
+      .then((data) => {
+        const mappedCoins = data.coins.map((coin) => ({
+          name: coin.item.name,
+          symbol: coin.item.symbol,
+          price: coin.item.price_btc,
+          logo: coin.item.large,
+        }));
+        setCoins(mappedCoins); // Update the coins array
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    // Fetch coins data when component is mounted
+    fetchCoins();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Trending</h1>
+      <div className="container">
+        {coins.map((coin) => (
+          <div key={coin.name}>
+            <h2>{coin.name}</h2>
+            <p>{coin.symbol}</p>
+            <p>{coin.price} BTC</p>
+            <img src={coin.logo} alt={coin.name} />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default HomePage;
